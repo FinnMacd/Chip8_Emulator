eb03d3f09488a6eb454981f4c5ee2d646adc26e1
afdmacdo <afdmacdo@uwaterloo.ca>
2019-01-02 17:15:36 -0500

    added opcode structure

--- src/Chip8.java
+++ src/Chip8.java
@@ -53,8 +53,6 @@ public class Chip8 extends JPanel implements Runnable{
 		ST = 0;
 		KEYBOARD = 0;
 		
-		display[1] = 1;
-		
 		chipThread.start();
 	}
 	
@@ -165,9 +163,139 @@ public class Chip8 extends JPanel implements Runnable{
 	
 	private void emulateCycle() {
 		
-		display[0]++;
-		display[1]*=2;
-		graphicsChanged = true;
+		
+		short opcode = (short)((unsigned(RAM[PC]) << 8) | unsigned(RAM[PC+1]));
+		PC+=2;
+		
+		int nnn = opcode&0xFFF;
+		int n = opcode&0xF;
+		int x = (opcode&0xF00)>>8;
+		int y = (opcode&0xF0)>>4;
+		int kk = opcode&0xFF;
+		
+		switch((opcode&0xF000)>>12) {
+		case 0x0:
+			if(kk == 0xE0);//clear display
+			else if(kk == 0xEE);//return from subroutine
+			else ;//jump to addr - not used
+			break;
+		case 0x1:
+			//jump to addr
+			break;
+		case 0x2:
+			//call subroutine addr
+			break;
+		case 0x3:
+			//skip instruction if Vx == kk
+			break;
+		case 0x4:
+			//skip if Vxx != kk
+			break;
+		case 0x5:
+			//skip if Vx == Vy
+			break;
+		case 0x6:
+			//set Vx = kk
+			break;
+		case 0x7:
+			//set Vx = Vx + kk
+		case 0x8:
+			switch(n) {
+			case 0x0:
+				//set Vx = Vy
+				break;
+			case 0x1:
+				//set Vx = Vx or Vy
+				break;
+			case 0x2:
+				//use and
+				break;
+			case 0x3:
+				//use xor
+				break;
+			case 0x4:
+				//vx = vx+vy, vf = carry
+				break;
+			case 0x5:
+				//vx = vx - vy, vf = not borrow
+				break;
+			case 0x6:
+				//vx = vx>>1, vf = underflow
+				break;
+			case 0x7:
+				//vx = vy - vx, vf = not borrow
+				break;
+			case 0xE:
+				//vx = vx<<1, vf = overflow
+				break;
+			default:
+				System.err.println("Invalid Opcode: " + opcode);
+				System.exit(1);
+			}
+			break;
+		case 0x9:
+			//skip if vx != vy
+			break;
+		case 0xA:
+			//I = nnn
+			break;
+		case 0xB:
+			//PC = nnn + V0
+			break;
+		case 0xC:
+			// vx = random byte & kk;
+			break;
+		case 0xD:
+			//draw stuff;
+			break;
+		case 0xE:
+			if(kk == 0x9E) {
+				//skip if key Vx is pressed
+			}else if(kk == 0xA1) {
+				//skip if key Vx is not pressed
+			}else {
+				System.err.println("Invalid Opcode: " + opcode);
+				System.exit(1);
+			}
+			break;
+		case 0xF:
+			switch (kk) {
+			case 0x07:
+				//Vx = delay timer
+				break;
+			case 0x0A:
+				// wait for key, put in vx
+				break;
+			case 0x15:
+				// set delay timer = vx
+				break;
+			case 0x18:
+				/// sound timer = vx
+				break;
+			case 0x1E:
+				// i += Vx
+				break;
+			case 0x29:
+				// i = location for digit vx
+				break;
+			case 0x33:
+				// store unsigned decimal representation of vx in i, i+1, i+2
+				break;
+			case 0x55:
+				//store v0 through vx in memory starting at i
+				break;
+			case 0x65:
+				//read from i into v0 through vx
+				break;
+			default:
+				System.err.println("Invalid Opcode: " + opcode);
+				System.exit(1);
+			}
+			break;
+		default:
+			System.err.println("Invalid Opcode: " + opcode);
+			System.exit(1);
+		}
 		
 	}
 	
@@ -186,5 +314,14 @@ public class Chip8 extends JPanel implements Runnable{
 		
 	}
 	
+	private int unsigned(short a) {
+		if(a<0)return ((int)1<<16) + a;
+		return a;
+	}
+	
+	private int unsigned(byte a) {
+		if(a<0)return ((int)1<<8) + a;
+		return a;
+	}
 	
 }
